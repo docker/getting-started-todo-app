e1698a12edc3a69d942b6ec56c7f49a098203f38
William Rende <william.jesusre@gmail.com>
2025-02-08 01:45:02 -0300

    Correção de Greetins

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,10 @@
-const GREETING = 'Hello world!';
-
+const GREETINGS = [ 
+    "Bem Vindo(a)!", 
+    "Todos a Bordo!", 
+    "Traçando Novoo Curso à Frente!",
+];
 module.exports = async (req, res) => {
-    res.send({
-        greeting: GREETING,
-    });
+res.send({ 
+greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )], });
+
 };
