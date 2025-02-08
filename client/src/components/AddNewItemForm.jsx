ddee764d93e7de8afe0ac9c0bd0b52c2976b54ea
William Rende <william.jesusre@gmail.com>
2025-02-08 11:01:47 -0300

    Alteração da variável 'placeholder' do elemento 'Form.Control'

--- client/src/components/AddNewItemForm.jsx
+++ client/src/components/AddNewItemForm.jsx
@@ -34,7 +34,7 @@ export function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={(e) => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="Cadastre Uma Nova Tarefa."
                     aria-label="New item"
                 />
                 <Button
