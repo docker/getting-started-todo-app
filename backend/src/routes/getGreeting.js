505a527f2f894d178ce88491ff82e3cec515ce5e
user001 <user001@nowhere.com>
2025-09-09 18:22:53 -0500

    For Great Justice! - Add more greetings

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,9 +1,18 @@
 const GREETINGS = [ 
+	"What happen?"
+	"Somebody set up us the bomb",
+	"Main screen turn on",
+	"Hello Mentlegen"
 	"All Your Base",
 	"Are Belong To Us",
 	"You have no Chance",
 	"Make your time",
-	"Move ZIG!",
+	"Move ZIG!"
+	"For great justice!",
+	"You know what you doing",
+	"How are you gentlemen!?",
+	"You!?"
+	"Take off every ZIG!!",
 ];
 
 module.exports = async (req, res) => {
