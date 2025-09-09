6ce731d8e929f9cc18fadaa5796143775da07b90
user001 <user001@nowhere.com>
2025-09-09 17:54:10 -0500

    Initial release

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,13 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [ 
+	"All Your Base",
+	"Are Belong To Us",
+	"You have no Chance",
+	"Make your time",
+	"Move ZIG!",
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor(Math.random() * GREETINGS.length )],
     });
 };
