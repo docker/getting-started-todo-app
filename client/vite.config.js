7f282c47fcd86652ea49800c5c976804cb5ac2b9
Kriaa89 <a.kriaa0605@myu.imset.tn>
2025-09-19 20:55:09 +0100

    chore: configure Vite server settings and proxy for API requests

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://backend:3000',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 });
